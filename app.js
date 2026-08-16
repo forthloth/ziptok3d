6444e3d67c44ae91c3029f7ef83271ec402e2fbf
MINGDA LIN <162898627+forthloth@users.noreply.github.com>
2026-08-16 12:09:18 +0800

    Refresh project page assets

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const explorerState = {
 };
 
 const metricData = { ShapeNet: {}, TRELLIS: {} };
-const ASSET_VERSION = "media4";
+const ASSET_VERSION = "media5";
 
 function assetUrl(path) {
   return `${path}?v=${ASSET_VERSION}`;
